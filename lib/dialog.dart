1b2641163f13055d8142330a80aa965e58a106ac
Pavel Dvořák <dvorapa@seznam.cz>
2018-03-09 18:16:51 +0100

    Add missing documentation

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -1,5 +1,13 @@
+// Copyright (c) 2014, Pavel Dvořák
+// All rights reserved. Use of this source code is governed by a
+// BSD 2-Clause license that can be found in the LICENSE file.
+
+/// Simple shorthand for importing all four dialogs at once.
 library dialog;
 
+/// Replaces default alert dialog.
 export "dialogs/alert.dart";
+/// Replaces default confirm dialog.
 export "dialogs/confirm.dart";
+/// Adds missing prompt dialog.
 export "dialogs/prompt.dart";
